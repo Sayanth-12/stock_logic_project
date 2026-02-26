25eaf844975aa9f7231c0aa3a3c1250c2f969f31
Sayanth A T <sayanthat73@gmail.com>
2026-02-26 21:51:40 +0530

    UI: Added icon to dashboard header for better visual clarity

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'sku', 'category', 'quantity', 'price', 'last_updated')
+    search_fields = ('name', 'sku')
+    list_filter = ('category',)
