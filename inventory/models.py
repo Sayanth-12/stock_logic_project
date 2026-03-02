d479fb3824663748a805a7d170c775e0b533366b
Sayanth A T <sayanthat73@gmail.com>
2026-03-03 00:09:17 +0530

    Refactor: Switched description to CharField per review

--- inventory/models.py
+++ inventory/models.py
@@ -20,7 +20,8 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     # ADD THIS LINE (The "Flawed" Code):
-    description = models.TextField(blank=True, null=True) 
+    # Change TextField to CharField
+description = models.CharField(max_length=255, blank=True, null=True) 
 
     def __str__(self):
         return self.name    
